614722d209e506d473459d376a03009019f22d88
lmatrecito <lmatrecito@users.noreply.github.com>
2020-09-01 13:43:44 -0400

    Update BisectionMethod.py
    Just working on my code. Not sure exactly if this is correct, but what I've done so far is the user can define a function and then the bisection method would be used for it... but I also need data so nevermind.

--- HW1_ExtremeScatteringEvents/BisectionMethod.py
+++ HW1_ExtremeScatteringEvents/BisectionMethod.py
@@ -5,4 +5,19 @@ Created on Tue Sep  1 13:12:42 2020
 @author: lizam
 """
 
+# Homework 1 Problem 1 - Write a library (i.e. a separate file that you can
+# call) for the three root-finding algorithms we discussed in class: 
+# 1. Bisection Method
+
+
+
+fx = input("Enter Function: ")
+
+def f():
+    print("f(x)= ", fx)
+f()
+
+
+    
+
 
