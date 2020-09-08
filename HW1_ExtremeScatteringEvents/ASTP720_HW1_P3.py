607a62a919a73197b2e22ee28d833b70d8416ae1
lmatrecito <lmatrecito@users.noreply.github.com>
2020-09-08 19:39:25 -0400

    Working on 3
    Just trying to figure this out... my root finding stuff isn't working :/ it's saying float division by 0 but I am not sure why!

--- HW1_ExtremeScatteringEvents/ASTP720_HW1_P3.py
+++ HW1_ExtremeScatteringEvents/ASTP720_HW1_P3.py
@@ -11,6 +11,8 @@ Created on Tue Sep  8 01:21:25 2020
 
 
 import math as m
+from ASTP720_HW1_P1 import Bisection
+import matplotlib.pyplot as plt
 
 # Given values
 r = 1                               # radius of observer's circular orbit (AU) 
@@ -28,6 +30,21 @@ a = 1.496e13
 N0 = 3.0856e16                                                        # (cm^2)
 re = 2.83e-13       
 
-# Solving for x'
-# xp = x(1 + (((l**2)*re*N0*D)/(m.pi*a**2)) * exp(-x/a)**2)
-
+# Solving for each value of x'
+xps = 1 + (((l**2)*re*N0*D)/(m.pi*a**2))                # Splitting up x' eqn.
+def f(xp, c):
+    def y(x):
+        return((x*((xps)*m.exp(-x/a)**2)) - xp)      # x' eqn. as a func. of y
+    return (y)
+# Creating a loop for 'orbit' of observer relative to lens
+q = []
+r = []
+for i in range(12):
+    xp = 1 + m.cos(i*m.pi/6)
+    r.append(xp)
+    z, q = Bisection(0, 2, f(xp, 1 - xps))
+    q.append(z)
+print(q)
+print(r)
+plt.clf()    
+    
