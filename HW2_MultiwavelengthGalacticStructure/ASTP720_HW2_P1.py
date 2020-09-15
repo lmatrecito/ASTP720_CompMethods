276d7dc48c2b9f075394ac9740c74b4f1831f588
lmatrecito <lmatrecito@users.noreply.github.com>
2020-09-15 19:29:15 -0400

    Update ASTP720_HW2_P1.py
    Added print to check if scripts were correct and did Simpson's Rule.

--- HW2_MultiwavelengthGalacticStructure/ASTP720_HW2_P1.py
+++ HW2_MultiwavelengthGalacticStructure/ASTP720_HW2_P1.py
@@ -18,21 +18,40 @@ def f(x):
 # Library that performs at least one derivative
 def der(f, x):       
     h = 0.000001           
-    df = (f(x + h) - f(x)) / (h)                # Definition of the derivative
+    df = (f(x+h) - f(x)) / (h)                # Definition of the derivative
     return(df)
-
+#n = der(f, 2)
+#print(n)
 
 # Three Integration Functions
 # 1. Midpoint Rule 
 
 # 2. Trapezoidal Rule
-def trap(g, a, b, n):
-    h = float(b - a) / n    # the higher the n, the better the convergence
-    integ = 0.0
-    integ += h * g(a)
+def trap(f, a, b, n):
+    h = (b-a) / n    # the higher the n, the better the convergence
+    ig = 0
+    ig += h * f(a)
     for i in range(1, n):
-        integ += h * g(a + i*h)
-    integ += h * g(b)
-    return(integ)
+        ig += h * f(a + i*h)
+    ig += h * f(b)
+    return(ig)
+#p = trap(f, 0, 1, 1000)
+#print(p)
 
 # 3. Simpson's Rule
+def simp(f, a, b, n):
+    h = (b-a) / n
+    k = 0
+    x = a + h
+    for i in range(1, n//2 + 1):    
+        k += 4*f(x)
+        x += 2*h
+        
+    x = a + 2*h
+    for i in range(1, n//2):
+        k += 2*f(x)
+        x += 2*h
+    return((h/3) * (f(a) + f(b) + k))
+#p = simp(f, 0, 1, 1000)
+#print(p)
+
