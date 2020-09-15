73c1b3c656c9ef0effd98087d600404249f698da
lmatrecito <lmatrecito@users.noreply.github.com>
2020-09-15 19:05:41 -0400

    Update ASTP720_HW2_P1.py
    Continuing HW2P1. Finished 1st part and added trapezoidal rule.

--- HW2_MultiwavelengthGalacticStructure/ASTP720_HW2_P1.py
+++ HW2_MultiwavelengthGalacticStructure/ASTP720_HW2_P1.py
@@ -7,23 +7,32 @@ Created on Tue Sep 15 16:58:17 2020
 
 
 # Homework 2 Problem 1 - Write a library that can perform at least one
-# derivative and at three integration functions i.e. midpoint rule, 
+# derivative & at least three integration functions i.e. midpoint rule, 
 # trapezoidal rule, and Simpson's rule
 
 
-# Derivative
 def f(x):
-    return x**2
+    return x**2                    # Can choose any function, this one is easy
 
-def derivative(func, xval):
-    h = 0.000001
-    df = (func(xval + h) - func(xval)) / (h)
+
+# Library that performs at least one derivative
+def der(f, x):       
+    h = 0.000001           
+    df = (f(x + h) - f(x)) / (h)                # Definition of the derivative
     return(df)
 
-p = derivative(f, 2)
-print(p)
-# Midpoint Rule 
 
-# Trapezoidal Rule
+# Three Integration Functions
+# 1. Midpoint Rule 
 
-# Simpson's Rule
+# 2. Trapezoidal Rule
+def trap(g, a, b, n):
+    h = float(b - a) / n    # the higher the n, the better the convergence
+    integ = 0.0
+    integ += h * g(a)
+    for i in range(1, n):
+        integ += h * g(a + i*h)
+    integ += h * g(b)
+    return(integ)
+
+# 3. Simpson's Rule
