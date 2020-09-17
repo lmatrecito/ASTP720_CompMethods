725e5c220d49fd608ad13456328a5a6845aff48f
lmatrecito <lmatrecito@users.noreply.github.com>
2020-09-17 18:09:37 -0400

    P3
    Doing this, might take a while. Will finish by tonight. Getting easier.

--- HW2_MultiwavelengthGalacticStructure/ASTP720_HW2_P3.py
+++ HW2_MultiwavelengthGalacticStructure/ASTP720_HW2_P3.py
@@ -9,3 +9,10 @@ Created on Tue Sep 15 17:17:12 2020
 # Homework 2 Problem 3 - Write a matrix library that includes a Matrix
 # class
 
+
+# Creating a Matrix Class
+class Matrix:
+    def _init_(self, dims, fill):
+        self.rows = dims[0]
+        self.cols = dims[1]
+        self.A = [[fill] * self.cols for i in range(self.rows)]
