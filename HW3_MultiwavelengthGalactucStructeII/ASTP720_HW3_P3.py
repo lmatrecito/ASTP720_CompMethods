aefd88f74acd98e77d838c320845a5380bfd2e44
lmatrecito <lmatrecito@users.noreply.github.com>
2020-09-24 18:36:17 -0400

    Update ASTP720_HW3_P3.py

--- HW3_MultiwavelengthGalactucStructeII/ASTP720_HW3_P3.py
+++ HW3_MultiwavelengthGalactucStructeII/ASTP720_HW3_P3.py
@@ -10,6 +10,7 @@ Created on Wed Sep 23 20:11:02 2020
 # Heun's method, and the explicit RK4 method
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Defining a function to use for the following ODE package
 def myfunc(x, y):
@@ -18,22 +19,19 @@ def myfunc(x, y):
     return(dy)
 # Function module used to evaluate equations
 def feval(funcName, *args):
-    return eval(funcName)(*args)
+    return(eval(funcName)(*args))
 
 
-# Euler's Methods
-
+# Euler's Methods:
 # Forward Euler's Method
 def forwardE(func, yinit, x_range, h):
     nODEs = len(yinit)                                        # Number of ODEs
     sub_int = int((x_range[-1] - x_range[0]) / h)    # Number of sub-intervals
-    
-    x = x_range[0] # Initializes variables x
-    y = yinit # Initializes variables y
+    x = x_range[0]                                   # Initializes variables x
+    y = yinit                                        # Initializes variables y
     # Creates arrays for solutions
     xsol = [x]
     ysol = [y[0]]
-    
     for i in range(sub_int):
         yp = feval(func, x, y)                               # Evaluates dy/dx
         for j in range(nODEs):
@@ -42,21 +40,45 @@ def forwardE(func, yinit, x_range, h):
         xsol.append(x)         
         for r in range(len(y)):
             ysol.append(y[r])             
-    return [xsol, ysol]
+    return([xsol, ysol])
 # Testing out Forward Euler
 # Setting parameters first, easier this way
 h = 0.001
 x = [0.0, 2.0]
-yinite = [4.0]
-[tse, yse] = forwardE('myfunc', yinite, x, h)
-# print([tse, yse])
+yinitef = [4.0]
+[tsef, ysef] = forwardE('myfunc', yinitef, x, h)
 
 
 # Backward Euler's Method
-def backwardE(f, x):
-    h = 0.001
-    df = (f(x) - f(x-h)) / (h)
-    return(df)
+# Defining a vector and multiplying by a scalar
+def mult(vector, scalar):
+    newv = [0]*len(vector)
+    for i in range(len(vector)):
+        newv[i] = vector[i]*scalar
+    return(newv)
+# Backward Euler's
+def backwardE(func, yinit, x_range, h):
+    nODEs = len(yinit)
+    sub_intervals = int((x_range[-1] - x_range[0]) / h)
+    x = x_range[0]
+    y = yinit
+    xsol = [x]
+    ysol = [y[0]]
+    for i in range(sub_intervals):
+        ypr = feval(func, x+h, y)
+        yp = mult(ypr, (1/(1+h)))
+        for j in range(nODEs):
+            y[j] = y[j] + h*yp[j]
+        x += h
+        xsol.append(x)
+        for r in range(len(y)):
+            ysol.append(y[r])  # Saves all new y's
+    return([xsol, ysol])
+# Testing out Backward Euler
+h = 0.001
+x = [0.0, 2.0]
+yiniteb = [4.0]
+[tseb, yseb] = backwardE('myfunc', yiniteb, x, h)
 # Checking to see if the classes work
 # dff = forwardE(f, 1)
 # dfb = backwardE(f, 1)
@@ -105,17 +127,16 @@ def RK4(func, yinit, x_range, h):
 # Setting parameters
 h = 0.001
 x = np.array([0.0, 2.0])
-yinit = np.array([1.0/10])
-[ts, ys] = RK4('myfunc', yinit, x, h)
+yinitr = np.array([1.0/10])
+[tsr, ysr] = RK4('myfunc', yinitr, x, h)
 # print([ts, ys])
 
 
 
 # Plotting to compare
-import matplotlib.pyplot as plt
-plt.plot(tse, yse, c='k', label='Forward Eulers')
-plt.plot(ts, ys, c='r', label='RK4')
-
+plt.plot(tsef, ysef, c='k', label='Forward Eulers')
+plt.plot(tseb, yseb, c='m', label='Backward Eulers')
+plt.plot(tsr, ysr, c='r', label='RK4')
 # Exact Solution
 dt = int((x[-1]-x[0])/h)
 t = [x[0]+i*h for i in range(dt+1)] 
@@ -129,4 +150,4 @@ plt.plot(t, yexact, 'b', label='Exact Solution')
 plt.ylim(0, .5)
 plt.xlim(0, 2)
 plt.legend()
-plt.title('Convergence')
+plt.title('ODE Methods')
